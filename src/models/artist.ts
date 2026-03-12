5351c5cf48f97cc085345f0f3f44a2d293e2f717
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-03-12 13:20:47 +0200

    add `models/artist.ts`

---
+++ src/models/artist.ts
@@ -0,0 +1,12 @@
+export interface Artist {
+    id: number;
+
+    name: string;
+    legalName: string;
+
+    bio?: string;
+    image?: string;
+    website?: string;
+    email?: string;
+    phone?: string;
+}
